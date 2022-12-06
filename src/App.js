24ea4267038f6d796d53a08635dd9bd4983881ef
Soja-K <sojaayilur2001@gmail.com>
2022-12-06 22:49:25 +0530

    React_Counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,75 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  
+
+  function incrementCounter() {
+    setCounter(counter + 1)
+  }
+  function decrementCounter(){
+    setCounter(counter-1)
+
+  }
+  function resetCounter(){
+    setCounter(0)
+  }
   return (
-    <div className="App">
+    <div  style={{
+      display:'flex',
+      flexDirection:'column',
+      alignItems:'center',
+      justifyContent:'center'
+    }}
+
+    
+    
+    
+    
+    
+    
+    className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <u> <h1>COUNTER APPLICATION</h1></u>
+        <strong><h1>{counter}</h1></strong>
+       <button
+        style={{
+          backgroundColor:'green',
+          color:'black',
+          border:'2px solid red',
+          padding:'20px',
+          margin:'20px',
+          fontWeight:'bold',
+          
+          
+        }}
+        
+        
+        
+        onClick={incrementCounter}>Increment</button>
+        <button 
+         style={{
+          backgroundColor:'yellow',
+          color:'black',
+          border:'2px solid blue',
+          padding:'20px',
+          margin:'20px',
+          fontWeight:'bold'
+        }}
+        onClick={decrementCounter}>Decrement</button>
+        <button 
+       
+        style={{
+         backgroundColor:'red',
+         color:'white',
+         border:'2px solid black',
+         padding:'20px',
+         margin:'20px',
+         fontWeight:'bold'
+       }}
+        onClick={resetCounter}>Reset</button>
+
       </header>
     </div>
   );
